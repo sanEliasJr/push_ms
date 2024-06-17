4138117e54ffb2c938790b2985ba30a2d620754d
Elias Junior <santanaeliasjunior.@gmail.com>
2024-06-17 18:18:31 -0300

    F0001 | Implementando API Server

---
+++ server/server/src/test/java/br/jus/tjba/api/push/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.jus.tjba.api.push.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
